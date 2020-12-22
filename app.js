d10aa8025ab2d985b5eb9f841d79d5c078aa33a8
Mason Molesky <masonmolesky@Masons-MacBook-Pro.local>
2020-12-21 23:41:00 -0500

    test1
    test 1

---
+++ app.js
@@ -0,0 +1,26 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+   
+    burger.addEventListener('click',() => {
+        //TOGGLE NAV
+        nav.classList.toggle('nav-active');
+
+        //ANIMATE LINKS
+    navLinks.forEach((link, index) => {
+        if (link.style.animation){
+            link.style.animation = '';
+
+        } else {
+            link.style.animation = 'navLinkFade 0.5s ease forwards ${'
+        }
+
+    });
+
+     //BURGER NAVIGATION 
+     burger.classList.toggle('toggle');
+    
+});
+}
+    navSlide();   
